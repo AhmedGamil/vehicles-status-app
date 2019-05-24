273d526a1baee96d7124006fadf8c88c12bec24a
Ahmed Gamil <Ahmed Gamil@LAPTOP-OH55T91O>
2019-05-24 06:59:18 +0400

    fix vehicleCascade

--- customer-vehicle-service/src/main/java/com/vehicle/data/store/customer/model/Vehicle.java
+++ customer-vehicle-service/src/main/java/com/vehicle/data/store/customer/model/Vehicle.java
@@ -1,12 +1,12 @@
 package com.vehicle.data.store.customer.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import org.hibernate.annotations.ForeignKey;
-
 
 @Entity
 public class Vehicle {
@@ -16,7 +16,7 @@ public class Vehicle {
 
 	private String regNumber;
 	
-	@ManyToOne(fetch = FetchType.EAGER)
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	@JoinColumn(name = "customer_id")
 	@ForeignKey(name="none")
 	private Customer customer;
